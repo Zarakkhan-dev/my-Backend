80e6cc9f5ae08da46333cdecfea206e21a856eb6
Zarakkhan-dev <119649492+Zarakkhan-dev@users.noreply.github.com>
2023-12-28 15:03:46 +0500

    a

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get("/",(req,res)=>{
 res.json({message:"hellow"});
 })
 
+app.post("/home",(req,res)=>{
+    const{name} = req.body;
+    console.log(name)
+    res.send("registered live")
+})
+
 app.listen(3000,()=>{
     console.log("port is listening ")
 })
